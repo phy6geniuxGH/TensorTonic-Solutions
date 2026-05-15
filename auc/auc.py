c4af593824ed0be108757c43317f5d0b6e48e3b3
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2026-05-16 03:52:39 +0800

    Add solution for auc

---
+++ auc/auc.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def auc(fpr, tpr):
+    """
+    Compute AUC (Area Under ROC Curve) using trapezoidal rule.
+    """
+    # Write code here
+    fpr = np.array(fpr)
+    tpr = np.array(tpr)
+    return np.trapezoid(tpr,fpr)
