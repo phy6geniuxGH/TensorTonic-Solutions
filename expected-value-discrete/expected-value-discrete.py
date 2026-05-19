d5f30e01a636ea864ad5d7cba359e5d0a1ccd963
Francis Santos Dela Cruz <32903993+phy6geniuxGH@users.noreply.github.com>
2026-05-20 03:15:19 +0800

    Update solution for expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -4,8 +4,10 @@ def expected_value_discrete(x, p):
     """
     Returns: float expected value
     """
-    if (np.sum(p) != 1):
-        raise ValueError("Sum of probability cannot be greater than 1")
-    else:
-        return np.sum(np.array(x)*np.array(p))
-     
+    if np.sum(p)!=1.0:
+        raise ValueError("p not sum to 1")
+
+    # Write code here
+    x = np.array(x)
+    p = np.array(p)
+    return np.dot(x,p) if np.sum(p)==1.0 else "ValueError"
